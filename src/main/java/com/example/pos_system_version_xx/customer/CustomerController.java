0b7a56a32c2f4041ec89376c8fad46eee809e8fb
Einar Helkkula <einarhelkkula@gmail.com>
2022-10-26 13:11:55 +0300

    Deleted the unnecessary customer related classes

--- src/main/java/com/example/pos_system_version_xx/customer/CustomerController.java
+++
@@ -1,7 +0,0 @@
-package com.example.pos_system_version_xx.customer;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CustomerController {
-}
--- src/main/java/com/example/pos_system_version_xx/customer/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pos_system_version_xx.customer;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository {
-}
